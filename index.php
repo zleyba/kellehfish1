67c90d46e3d90e5239ec4a73d89d8c5f17cce164
zleyba <mtvzach@gmail.com>
2016-03-08 23:00:02 -0700

    added index and support files/structure

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE HTML>
+<!--
+ * Created by PhpStorm.
+ * User: zleyba
+* Date: 2/2/16
+* Time: 10:24 AM
+-->
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<?php require_once(dirname(__DIR__) . "lib/head-tags.html"); ?>
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Didot' rel='stylesheet' type='text/css'>
+
+		<!-- loads gallery support files-->
+		<link rel="stylesheet" href="/css/swipebox.css">
+		<script src="lib/jquery-2.1.0.min.js"></script>
+		<script src="js/jquery.swipebox.js"></script>
+
+		<script type="text/javascript">
+			;( function( $ ) {
+
+				$( '.swipebox' ).swipebox();
+
+			} )( jQuery );
+		</script>
+		<!--end gallery support-->
+
+		<!--Nav bar Scripts-->
+
+		<!--Navbar Transformation script-->
+		<script type="text/javascript" src="js/navtransform.js"></script>
+
+
+
+
+		<!--main style sheet-->
+		<link rel="stylesheet" href="css/style.css" />
+
+		<title>The Personal Website of Illustrator Kelly Williams</title>
+
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<nav class="navbar nav">
+
+					<div class="title">kellehfish</div>
+
+					<div class="navbar-header">
+
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+
+					</div>
+					<div class="ptitle">kellehfish</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="navbar-collapse-1">
+						<ul class="nav">
+							<li class="item"><a href="#">a b o u t</a></li>
+							<li class="item"><a href="#">w o r k</a></li>
+							<li class="item"><a href="#">c o n t a c t</a></li>
+							<li class="item"><a href="#">s h o p</a></li>
+						</ul>
+
+
+					</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+		</nav>
+		</header>
+
+			<main>
+
+				<div class="container" id="box-container">
+
+					<div class="row">
+						<div class="col-md-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non vulputate diam. Cras tincidunt eros lorem, ut
+								convallis mi sodales eget. Sed imperdiet, elit congue iaculis placerat, leo diam vestibulum ipsum, non efficitur
+								purus ipsum sit amet risus. Duis vel dui quis ipsum volutpat mollis. Pellentesque varius sem vel nulla auctor,
+								eu fringilla elit dapibus. Pellentesque lacinia, elit vitae commodo finibus, nisl orci suscipit nibh, sit
+								amet aliquam sem sem id mauris. Sed venenatis porttitor augue ac egestas. Etiam ac odio enim. Suspendisse
+								tincidunt ut ligula nec tempor.</p>
+						</div>
+					</div>
+
+
+			</main>
+		</div>
+
+
+
+		<footer>
+			<div class="container">
+
+
+				(c) Kelly Williams 2016
+			</div>
+		</footer>
+
+
+	</body>
